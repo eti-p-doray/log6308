28f9435e0593afd4d1bf9409910b32cfe0e89532
Bettorun <youri.barsalou@gmail.com>
2017-11-13 22:52:42 -0500

    Output to csv after saving checkpoint

--- netflix_utils.py
+++ netflix_utils.py
@@ -3,6 +3,7 @@ import logging
 import tensorflow as tf
 import netflix_data
 import os
+import csv
 from tensorflow.contrib.tensorboard.plugins import projector
 
 class NetflixUtils(object):
@@ -91,6 +92,8 @@ class NetflixUtils(object):
                             help='Total number of iterations')
         parser.add_argument('--logdir', default="log/" + self.model_name_,
                             help='Directory where logs should be written')
+        parser.add_argument('--out_type', default=None,
+                            help='Type of output to produce in logs')
 
         # Configuring Log severity for future printing
         logging.basicConfig(level=logging.DEBUG)
@@ -144,12 +147,32 @@ class NetflixUtils(object):
         self.training_set_ = netflix_data.DataSet.fromfile(self.args_.input)
         self.test_set_ = netflix_data.DataSet.fromfile(self.args_.test_set)
 
+    def save_perfo(self, values, clear = False):
+        if self.args.out_type is not None:
+            if self.args.out_type == 'csv':
+                logging.debug("before open")
+                if (clear):
+                    logging.debug('clearing')
+                    mode = 'w'
+                else:
+                    mode = 'a'
+                with open(os.path.join(self.args_.logdir, 'performance.csv'), mode, newline='') as csvfile:
+                    csvwriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                    if (clear):
+                        csvwriter.writerow(("i", "Accuracy", "RMSE", "Loss", "Is_Test"))
+                    logging.debug("after header")
+                    for value in values:
+                        csvwriter.writerow(value)
+
     def train_model(self, sess, train_step, accuracy, rmse, loss,
                     train_data_update_freq, test_data_update_freq,
                     sess_save_freq, batch_size):
 
         train_batch_iter = self.training_set.iter_batch(batch_size)
         logging.debug('Training model')
+        perfo_results = []
+        first_write = True
         while self.global_step_.eval(sess) < self.args_.n_iter:
             batch = next(train_batch_iter) # next batch of data to train on.
 
@@ -168,6 +191,7 @@ class NetflixUtils(object):
                                                     self.dates_ : batch.dates,
                                                     self.ratings_: batch.ratings})
                 logging.info(str(i) + ": accuracy:" + str(a) + " loss: " + str(l) + " rmse: " + str(m))
+                perfo_results.append((i, a, m, l, False))
 
             # compute test values for visualisation
             if i % test_data_update_freq == 0:
@@ -178,8 +202,12 @@ class NetflixUtils(object):
                                               self.ratings_: self.test_set.ratings})
                 logging.info(str(i) + ": ********* epoch " + str(i) + " ********* test accuracy:" + str(a) + " test loss: " + str(
                     l) + " rmse: " + str(m))
+                perfo_results.append((i, a, m, l, True))
 
             # Saving training progress
             if i % sess_save_freq == 0:
                 logging.debug('Saving model')
                 self.saver_.save(sess, os.path.join(self.args_.logdir, self.model_name_+".ckpt"), global_step=i)
+                self.save_perfo(perfo_results, first_write)
+                perfo_results = []
+                first_write = False
