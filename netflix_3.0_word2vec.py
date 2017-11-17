7c3bd0db2f4e5c6a46248465b83e2cd24d65e38d
Bettorun <youri.barsalou@gmail.com>
2017-11-17 11:53:43 -0500

    Completing the first version of word2vec use.

--- netflix_3.0_word2vec.py
+++ netflix_3.0_word2vec.py
@@ -25,25 +25,25 @@ def preprocess_word_nt(movie_titles):
                 nt[word] = 1
     return nt
 
-def preprocess_title_embeddings(model, movie_titles, nt):
-    embeddings = []
-    for movie_title in movie_titles:
-        embedding = []
+def preprocess_title_embeddings(model, movie_titles, nt, ncol):
+    embeddings = numpy.zeros((len(movie_titles), ncol))
+    for i, movie_title in enumerate(movie_titles):
+        embedding = numpy.zeros((ncol))
         length = 0
         for word in movie_title:
             if word in model.wv.vocab:
-                word_vector = model[word].tolist()
+                word_vector = model[word][0:ncol]
                 tfidf = log10(len(movie_titles) / nt[word])
-                embedding = embedding + [x * tfidf for x in word_vector]
+                embedding = embedding + tfidf * word_vector
                 length = length + 1
-        embedding = [x / length for x in embedding]
-        embeddings.append(embedding)
+        embedding = embedding / length
+        embeddings[i, :] = embedding
 
     return embeddings
 
 def preprocess_get_titles(utils):
     if utils.args.gen_movie_titles:
-        all_titles = {}
+        all_titles = []
         with open('nf_prize_dataset/movie_titles.tsv') as f:
             first = True
             for line in f:
@@ -51,31 +51,21 @@ def preprocess_get_titles(utils):
                     first = False
                     continue
                 words = line.split()
-                movie_id = words.pop(0)
+                words.pop(0)
                 words.pop(0) #remove year
                 title = " ".join(words)
-                all_titles[int(movie_id)] = title
+                all_titles.append(title)
 
-        training_movie_titles = []
-        test_movie_titles = []
-        for movie_id in utils.training_set.movie_ids:
-            # + 1 because we offseted it by minus one on npy creation
-            training_movie_titles.append(all_titles[movie_id + 1])
-        training_array = numpy.array(training_movie_titles, dtype=numpy.string_)
-        numpy.save(utils.args.input + '.titles.npy', training_array)
-        for movie_id in utils.test_set.movie_ids:
-            # + 1 because we offseted it by minus one on npy creation
-            test_movie_titles.append(all_titles[movie_id + 1])
-        test_array = numpy.array(test_movie_titles, dtype=numpy.string_)
-        numpy.save(utils.args.test_set + '.titles.npy', test_array)
+        title_array = numpy.array(all_titles, dtype=numpy.unicode_)
+        numpy.save('nf_prize_dataset/nf_titles.npy', title_array)
 
         logging.debug('Titles generated and saved')
 
     else:
-        training_array = numpy.load(utils.args.input + '.titles.npy')
-        test_array = numpy.load(utils.args.test_set + '.titles.npy')
+
+        title_array = numpy.load('nf_prize_dataset/nf_titles.npy')
         logging.debug('Title loaded from existing')
-    return training_array.tolist() + test_array.tolist()
+    return title_array.tolist()
 
 
 def main(argv):
@@ -89,17 +79,20 @@ def main(argv):
     ## Description of the TensorFlow model.
     #Constants
     user_embedding_size = 60
-    movie_embedding_size = 60
 
     utils.init_tensorflow()
 
     movie_titles = preprocess_get_titles(utils)
 
     model = KeyedVectors.load_word2vec_format('./word2vec/GoogleNews-vectors-negative300.bin', binary=True)
-    title_embedding_value = preprocess_title_embeddings(model, movie_titles, preprocess_word_nt(movie_titles))
-    title_embeddings = tf.constant(numpy.asarray(title_embedding_value))
-    #embedded_titles = tf.gather(title_embeddings, utils.movie_ids)
+    title_embedding_value = preprocess_title_embeddings(model, movie_titles, preprocess_word_nt(movie_titles), user_embedding_size)
+
     logging.debug('done with preprocess')
+
+    title_embeddings = tf.get_variable("title_embeddings", initializer=tf.constant(numpy.asarray(title_embedding_value), dtype=numpy.float32), trainable=False)
+    embedded_titles = tf.gather(title_embeddings, utils.movie_ids)
+
+    logging.debug("title embedding initialized")
 
 
     #Trainable embeddings for users. Tensor format n_user x user_embedding_size.
@@ -109,8 +102,8 @@ def main(argv):
 
     #Trainable embeddings for movies. Tensor format n_movie x movie_embedding_size.
     #Initialized randomly accorded to a truncated normal distribution
-    movie_embeddings = tf.get_variable("movie_embeddings", initializer=tf.truncated_normal([netflix_data.MOVIE_COUNT, movie_embedding_size], stddev=0.1), trainable=True)
-    embedded_movies = tf.gather(movie_embeddings, utils.movie_ids)#Loads embeddings of currently treated movies.
+    #movie_embeddings = tf.get_variable("movie_embeddings", initializer=tf.truncated_normal([netflix_data.MOVIE_COUNT, movie_embedding_size], stddev=0.1), trainable=True)
+    #embedded_movies = tf.gather(movie_embeddings, utils.movie_ids)#Loads embeddings of currently treated movies.
 
     # Biases
     user_bias = tf.get_variable("user_bias", initializer=tf.zeros([netflix_data.USER_COUNT]))
@@ -118,7 +111,7 @@ def main(argv):
     B = numpy.mean(utils.training_set.ratings)
 
     # Layer description
-    Y = tf.reduce_sum(tf.multiply(embedded_movies, embedded_users), 1) + tf.gather(user_bias, utils.user_ids) + tf.gather(movie_bias, utils.movie_ids) + B
+    Y = tf.reduce_sum(tf.multiply(embedded_titles, embedded_users), 1) + tf.gather(user_bias, utils.user_ids) + tf.gather(movie_bias, utils.movie_ids) + B
 
     #Error calculation
     mse = tf.reduce_mean(tf.square(tf.cast(utils.ratings, tf.float32) - Y))
@@ -126,7 +119,7 @@ def main(argv):
     # Loss function to minimize
     loss = (mse + REGULARIZATION_FACTOR*(
            tf.reduce_mean(tf.reduce_sum(tf.square(embedded_users), 1)) +
-           tf.reduce_mean(tf.reduce_sum(tf.square(embedded_movies), 1)) +
+           tf.reduce_mean(tf.reduce_sum(tf.square(embedded_titles), 1)) +
            tf.reduce_mean(tf.square(tf.gather(user_bias, utils.user_ids))) +
            tf.reduce_mean(tf.square(tf.gather(movie_bias, utils.movie_ids)))))
 
@@ -145,7 +138,7 @@ def main(argv):
     sess.run(init)
 
     utils.restore_existing_checkpoint(sess)
-    utils.setup_projector(movie_embeddings.name)
+    utils.setup_projector(title_embeddings.name)
 
 
     ############################################################################
